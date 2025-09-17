47a4c641624ad0c6f23709464477be181797f3dc
Johnkanelorchano <kanelorchano@gmail.com>
2025-09-17 11:53:53 +0800

    Add files via upload
    The sum of x and y

---
+++ main (2).cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+   
+    // cout << "this is a comment";
+    /*This is what i did during my overtime;
+    1,
+    2,
+    3,
+    */
+    //cout << "This is a comment" << endl;
+    
+    int x, y;
+    
+    cout << "Enter the vlue of x =";
+    cin >> x;
+     cout << "Enter the vlue of y =";
+     cin >> y;
+     
+     int sum = x + y;
+     
+     cout << "The sum of x and y is ";
+     cout << sum << endl;
+     
+     return 0;
+     
+    
+}
